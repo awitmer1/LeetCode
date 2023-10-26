d604609ae0c5be5e158ed7916d5e2c0c5adad3f0
Andrew Witmer <witmer.a@gmail.com>
2023-10-25 17:14:48 -0700

    first commit

---
+++ 1. Two Sum - Easy.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+
+my_solution = Solution()
+
+result = my_solution.twoSum([2,7,11,15], 9)
+print(result)
